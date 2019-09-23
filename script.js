e8877aff303a0349240dd53b1680ec98e9a07013
harucoder <55718779+harucoder@users.noreply.github.com>
2019-09-23 15:51:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 //Refer to https://scrapbox.io/scrasobox/%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%83%9A%E3%83%BC%E3%82%B8%E3%82%92%E4%BD%9C%E6%88%90%EF%BC%88UserScript%E7%89%88%EF%BC%89
+//Include "script.js" in your Userpage
